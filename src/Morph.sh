06be25bef0bc7338098e61177bfd11882323f4c7
Ishan Sharma <sharmaishan747@gmail.com>
2021-04-13 13:37:16 +0530

    Shell Script and Makefile Changes

--- src/Morph.sh
+++ src/Morph.sh
@@ -1,9 +1,8 @@
 #! /bin/bash
 
-echo 'Enter the Image1 Location: '
+echo 'Enter the Image1 Location and Name (Example: ./Image1.png OR ../images/Image1.png): '
 read Image1
-echo 'Enter the Image2 Location: '
+echo 'Enter the Image2 Location and Name (Example: ./Image2.png OR ../images/Image2.png): '
 read Image2
 
-javac Editor.java
-java Editor Image1 Image2 Results.txt
+javac Editor.java && java Editor $Image1 $Image2 Results.txt
